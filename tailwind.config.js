255f6a8163bb6eff218c6ff52d680e89a0066796
Liza <lizavetayalak2000@gmail.com>
2023-07-07 17:03:04 +0200

    feat: add header, nav & part of main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+    colors: {
+      primary : '#06b6d4',
+      text: '#1f2937',
+      'text-white' : '#fafafa',
+    },
+    fontFamily: {
+      sans: ['Roboto Condensed']
+    }
   },
   plugins: [],
 }
